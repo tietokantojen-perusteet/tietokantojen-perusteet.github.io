4cb20ec6718e716d5e77c47f146801d4ce01bbe6
avihavai <avihavai@XX.helsinki.fi>
2015-09-13 21:45:41 +0300

    draft for giving feedback on SQL questions

--- js/directives.js
+++ js/directives.js
@@ -33,28 +33,40 @@ myAppModule.directive('tableInfo', function() {
 });
 
 myAppModule.directive('query', function() {
-  return {
+    return {
       restrict: 'E',
       templateUrl: 'templates/query-template.html',
       scope: {
           db: '=',
           query: '@',
           editable: '@',
-          done: '=?' // done should be optional
+          done: '=?',
+	  valid: '@?'
       },
-      controller: function($scope){
+      controller: function($scope) {
           $scope.run_query = function(query) {
               try {
-                  $scope.error = null
+		  console.log("Running query " + query);
+		  $scope.done = false;
+		  $scope.validMessage = null;
+                  $scope.error = null;
 
 		  if(!query || query.length == 0) {
+		      $scope.error = "Annettiin tyhjä kysely -- ei suoritettavaa.";
 		      return;
 		  }
 
 		  if (query.toUpperCase().indexOf("DELETE") > -1 || 
 		      query.toUpperCase().indexOf("INSERT") > -1) {
 		      $scope.db.run(query);
-		      $scope.done = true;
+
+		      if(!$scope.valid) {
+			  $scope.done = true;
+		      } else {
+			  console.log("TODO: validate update");
+		      }
+		      
+
 		      return;		      
 		  }
 
@@ -72,12 +84,82 @@ myAppModule.directive('query', function() {
                   }
                   
 		  $scope.columns = Object.keys($scope.rows[0]);
-                  $scope.done = true
+		  
+		  if(!$scope.valid) {
+                      $scope.done = true
+		  } else {
+		      console.log("Validating against result " + query);
+      		      var testStmt = $scope.db.prepare($scope.valid);
+                      testStmt.getAsObject();
+                      testStmt.bind();
+		      
+		      var testRows = [];
+                      
+		      while(testStmt.step()) {
+			  var row = testStmt.getAsObject();
+			  testRows.push(row);
+                      }
+
+		      var testColumns = Object.keys(testRows[0]);
+
+		      // test cases
+		      // 1. onko sarakkeiden lukumäärä toivottu
+		      console.log("Test case 1");
+		      if(testColumns.length != $scope.columns.length) {
+			  $scope.validMessage = [];
+			  $scope.validMessage.push("Kyselyn vastauksessa tulisi olla " + testColumns.length + " saraketta, mutta nyt niitä oli " + $scope.columns.length);
+			  return;
+		      }
+
+		      console.log("Test case 2");
+		      // 2. ovatko sarakkeet nimetty toivotulla tavalla
+		      for (var i = 0; i < testColumns.length; i++) {
+			  if(testColumns[i].valueOf() == $scope.columns[i].valueOf()) {
+			      continue;
+			  }
+
+			  $scope.validMessage = [];
+			  $scope.validMessage.push("Sarakkeen " + (i + 1) + " pitäisi olla " + testColumns[i] + ", mutta nyt se oli " + $scope.columns[i]);
+			  return;
+		      }
+		      
+		      console.log("Test case 3");
+ 		      // 3. onko rivien lukumäärä toivottu
+		      if(testRows.length != $scope.rows.length) {
+			  $scope.validMessage = [];
+			  $scope.validMessage.push("Kyselyn vastauksessa tulisi olla " + testRows.length + " riviä, mutta nyt niitä oli " + $scope.rows.length);
+			  return;
+		      }
+
+		      // 4. ovatko rivit järjestyksessä
+		      // TODO: myöh
+
+		      console.log("Test case 5");
+		      testRows.length != $scope.rows.length
+		      // 5. onko n:s datarivi ok
+		      for (var i = 1; i < testRows.length; i++) {
+			  for (var j = 0; j < testColumns.length; j++) {
+			      if(testRows[i][testColumns[j]].valueOf() == $scope.rows[i][testColumns[j]].valueOf()) {
+				  continue;
+			      }
+
+			      $scope.validMessage = [];
+			      $scope.validMessage.push("Virhe vastauksen rivillä " + i + ". Odotettiin, että arvot olisivat seuraavat:");
+
+			      for (var k = 0; k < testColumns.length; k++) {
+				  $scope.validMessage.push("  " + testColumns[k] + ": " + testRows[i][testColumns[k]]);
+			      }
+
+			      return;
+			  }
+		      }
+
+		      $scope.done = true;
+		  } 
               }
               catch(err) {
                   $scope.error = err.toString();
               }
-
           };
 
           $scope.clear = function () {
